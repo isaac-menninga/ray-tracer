84bc532eab04022c3711fb9b970eb0f1f6a3baf0
Isaac Menninga <igmenninga@gmail.com>
2022-08-12 16:47:02 -0700

    use outward normals always

--- src/sphere.rs
+++ src/sphere.rs
@@ -55,10 +55,20 @@ impl Sphere {
             if t > 0.0003 {
                 let intersection = r.line_to_p(t);
 
+                let normal = (intersection - self.center).to_unit_vector();
+                let mut outward_normal = normal;
+                let front_face = r.direction.dot(outward_normal) < 0.0;
+
+                if front_face {
+                    outward_normal = outward_normal;
+                } else {
+                    outward_normal = -outward_normal;
+                }
+
                 return Some(Hit {
                     t: t,
                     p: intersection,
-                    normal: (intersection - self.center).to_unit_vector(),
+                    normal: outward_normal,
                     material: self.material
                 })
             } else {
